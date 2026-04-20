c21fea5497c9ae60ce326f30158baddc8c0532ef
Obama <elchinotedeaguastermales@gmail.com>
2026-04-19 20:27:39 -0500

    little changes 0.2

--- src/sections/PortfolioSection.tsx
+++ src/sections/PortfolioSection.tsx
@@ -17,7 +17,7 @@ const projects = [
     title: 'Bolones Picapiedra',
     tag: 'Video Prom',
     image: 'https://ik.imagekit.io/ObamaRS12/Frame%20House/FH.jpg',
-    videoUrl: 'https://res.cloudinary.com/dolxglacq/video/upload/q_auto/f_auto/v1776644599/Ojos-asi_avh7b2.mp4'
+    videoUrl: 'https://res.cloudinary.com/dolxglacq/video/upload/q_auto/f_auto/v1776647903/Video_Prom_zx0qjx.mp4'
   }
 ];
 
